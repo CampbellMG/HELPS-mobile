8fdd4bf6d8862cd40cbbbbaf9399472f17e31a44
CampbellMG <campbell.gorman@gmail.com>
2019-09-15 22:17:16 +1000

    [WIP] Added login screen

--- src/components/screens/Login.tsx
+++ src/components/screens/Login.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image, TextInput, TouchableOpacity} from 'react-native';
+import {PRIMARY} from "../../res/Colours";
 
 export class Login extends React.Component<{}, {}> {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Open up App.tsx to start working on your app!</Text>
+                <View style={{
+                    borderWidth: 5,
+                    borderColor: PRIMARY,
+                    paddingVertical: 30,
+                    paddingHorizontal: 60,
+                    borderRadius: 1000,
+                    alignSelf: 'center',
+                    marginBottom: 128
+                }}>
+                    <Image source={require('../../../assets/uts.png')} style={{
+                        height: 125,
+                        width: 74
+                    }}/>
+                </View>
+                <Text style={{fontSize: 16}}>Email</Text>
+                <TextInput style={{
+                    borderWidth: 2,
+                    borderColor: PRIMARY,
+                    borderRadius: 10,
+                    marginBottom: 32,
+                    marginTop: 8,
+                    fontSize: 16,
+                    padding: 8,
+                    paddingHorizontal: 16
+                }}/>
+                <Text style={{fontSize: 16}}>Password</Text>
+                <TextInput  style={{
+                    borderWidth: 2,
+                    borderColor: PRIMARY,
+                    borderRadius: 10,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginBottom: 32,
+                    marginTop: 8,
+                    fontSize: 16,
+                    padding: 8,
+                    paddingHorizontal: 16
+                }}/>
+                <TouchableOpacity style={{backgroundColor: PRIMARY, padding: 15, borderRadius: 10, marginTop: 32}}>
+                    <Text style={{fontSize: 30, color: 'white', textAlign: 'center'}}>Login</Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -15,7 +56,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
+        alignItems: 'stretch',
         justifyContent: 'center',
+        padding: 32
     },
 });
