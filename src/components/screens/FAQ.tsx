2ef0f67e574e3dfead840e4cb7ed82030459ebc3
Campbell <campbell.m.gorman@student.uts.edu.au>
2019-09-20 16:19:35 +1000

    Updated redux message model and added basic FAQ component

--- src/components/screens/FAQ.tsx
+++ src/components/screens/FAQ.tsx
@@ -1,21 +1,60 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {TouchableOpacity, View, WebView} from 'react-native';
+import {connect} from "react-redux";
+import {AppState} from "../../types/store/StoreTypes";
+import {ContentType, FAQDispatchProps, FAQProps, FAQState, FAQStateProps} from "../../types/components/FAQ";
+import {ThunkDispatch} from 'redux-thunk';
+import {fetchMessages} from "../../store/actions/MessageActions";
 
-export class FAQ extends React.Component<{}, {}> {
+class FAQ extends React.Component<FAQProps, FAQState> {
+
+    constructor(props: FAQProps) {
+        super(props);
+
+        this.state = {
+            selectedContentType: 'PROGRAMS'
+        }
+    }
+
+
+    componentDidMount(): void {
+        this.props.retrieveMessages();
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Open up App.tsx to start working on your app!</Text>
+            <View>
+                {this.renderInformationBlock("PROGRAMS")}
+                {this.renderInformationBlock("FAQ")}
             </View>
         );
     }
+
+    private renderInformationBlock(type: ContentType) {
+        const {programs, FAQ} = this.props.messages;
+        const {selectedContentType} = this.state;
+
+        return (
+            <View>
+                <TouchableOpacity onPress={() => this.setState({selectedContentType: type})}>
+                    {type}
+                </TouchableOpacity>
+                {selectedContentType === type &&
+                <WebView originWhitelist={['*']}
+                         source={{html: type === "PROGRAMS" ? programs : FAQ}}/>}
+
+            </View>
+        )
+    }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+const mapStateToProps = (state: AppState): FAQStateProps => ({
+    messages: state.message.indexedMessages,
+    error: state.message.error
 });
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<{}, {}, any>): FAQDispatchProps => ({
+    retrieveMessages: () => dispatch(fetchMessages())
+});
+
+export default connect<FAQStateProps, FAQDispatchProps, {}, AppState>(mapStateToProps, mapDispatchToProps)(FAQ)
--- src/types/store/MessageReducerTypes.ts
+++ src/types/store/MessageReducerTypes.ts
@@ -1,6 +1,7 @@
-import { Message } from '../model/Message';
+import {Message, MessageDictionary} from '../model/Message';
 
 export interface MessageState {
     error?: string
     messages: Message[]
+    indexedMessages: MessageDictionary
 }
