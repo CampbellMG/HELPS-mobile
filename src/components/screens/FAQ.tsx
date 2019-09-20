71166af69adf077913d5083f1c473d80fc2a9907
Campbell <campbell.m.gorman@student.uts.edu.au>
2019-09-20 16:56:31 +1000

    Updated FAQ list to use Flat list

--- src/components/screens/FAQ.tsx
+++ src/components/screens/FAQ.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {TouchableOpacity, View, WebView} from 'react-native';
+import React, {ComponentType} from 'react';
+import {FlatList, ListRenderItem, ListRenderItemInfo, TouchableOpacity, View, WebView} from 'react-native';
 import {connect} from "react-redux";
 import {AppState} from "../../types/store/StoreTypes";
 import {ContentType, FAQDispatchProps, FAQProps, FAQState, FAQStateProps} from "../../types/components/FAQ";
@@ -24,24 +24,25 @@ class FAQ extends React.Component<FAQProps, FAQState> {
     render() {
         return (
             <View>
-                {this.renderInformationBlock("PROGRAMS")}
-                {this.renderInformationBlock("FAQ")}
+                <FlatList data={['PROGRAMS', 'FAQ']}
+                          renderItem={this.renderInformationBlock}/>
             </View>
         );
     }
 
-    private renderInformationBlock(type: ContentType) {
+    private renderInformationBlock = (data: ListRenderItemInfo<ContentType>) => {
+        const {item} = data;
         const {programs, FAQ} = this.props.messages;
         const {selectedContentType} = this.state;
 
         return (
             <View>
-                <TouchableOpacity onPress={() => this.setState({selectedContentType: type})}>
-                    {type}
+                <TouchableOpacity onPress={() => this.setState({selectedContentType: item})}>
+                    {item}
                 </TouchableOpacity>
-                {selectedContentType === type &&
+                {selectedContentType === item &&
                 <WebView originWhitelist={['*']}
-                         source={{html: type === "PROGRAMS" ? programs : FAQ}}/>}
+                         source={{html: item === "PROGRAMS" ? programs : FAQ}}/>}
 
             </View>
         )
